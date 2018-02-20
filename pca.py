e85a0abc8a25bac3d7a7fdd0f01ab7410a57c5c6
Robert <colgan@cs.columbia.edu>
2018-02-20 15:13:09 -0500

    Final

--- pca.py
+++ pca.py
@@ -53,10 +53,16 @@ def reconstructed_image(D, c, num_coeffs, X_mean, n_blocks, im_num):
     c_im = c[ : num_coeffs , n_blocks * n_blocks * im_num : n_blocks * n_blocks * (im_num + 1)]
     D_im = D[ : , : num_coeffs]
 
-    # TODO: Enter code below for reconstructing the image X_recon_img
-    # ......................
-    # ......................
-    # X_recon_img = ........
+    sz = X_mean.shape
+    recon = np.dot(D_im, c_im)
+    X_recon_img = np.empty([n_blocks*sz[0], n_blocks*sz[1]], dtype="float32")
+    for i in range(0, n_blocks):
+        for j in range(0, n_blocks):
+            curBlock = recon[:, i*n_blocks + j]
+            curBlock = curBlock.reshape(sz)
+            curBlock += X_mean
+            X_recon_img[ i*sz[0] : (i+1)*sz[0] , j*sz[1] : (j+1)*sz[1] ] = curBlock
+
     return X_recon_img
 
 
@@ -90,11 +96,12 @@ def plot_reconstructions(D, c, num_coeff_array, X_mean, n_blocks, im_num):
         im_num: Integer
             index of the image to visualize
     '''
+
     f, axarr = plt.subplots(3, 3)
     for i in range(3):
         for j in range(3):
             plt.axes(axarr[i, j])
-            plt.imshow(reconstructed_image(D, c, num_coeff_array[i * 3 + j], X_mean, n_blocks, im_num))
+            plt.imshow(reconstructed_image(D, c, num_coeff_array[i * 3 + j], X_mean, n_blocks, im_num), cmap=cm.Greys_r)
 
     f.savefig('output/pca_{0}_im{1}.png'.format(n_blocks, im_num))
     plt.close(f)
@@ -118,9 +125,18 @@ def plot_top_16(D, sz, imname):
     imname: string
         name of file where image will be saved.
     '''
-    # TODO: Obtain top 16 components of D and plot them
 
-    raise NotImplementedError
+    f, axarr = plt.subplots(4, 4)
+    for i in range(16):
+        cur_comp = D[:, i]
+        cur_comp_scaled = cur_comp * 1/max(cur_comp)
+        cur_comp_scaled = cur_comp_scaled.reshape((sz, sz))
+
+        plt.axes(axarr[i // 4, i % 4])
+        plt.imshow(cur_comp_scaled, cmap=cm.Greys_r)
+
+    f.savefig(imname)
+    plt.close(f)
 
 
 def main():
@@ -128,7 +144,18 @@ def main():
     Read here all images(grayscale) from Fei_256 folder into an numpy array Ims with size (no_images, height, width).
     Make sure the images are read after sorting the filenames.
     '''
-    # TODO: Read all images into a numpy array of size (no_images, height, width)
+
+    no_images = 200
+    height = 256
+    width = 256
+    all_imgs = np.empty([no_images, height, width], dtype="float32")
+
+    for root, dirs, files in os.walk('Fei_256/'):
+        files = [im_file for im_file in files if '.jpg' in im_file]
+    for i, im_file in enumerate(sorted(files)):
+        im = Image.open('Fei_256/' + im_file)
+        im.load()
+        all_imgs[i, :, :] = np.asarray(im, dtype="float32")
 
     szs = [8, 32, 64]
     num_coeffs = [range(1, 10, 1), range(3, 30, 3), range(5, 50, 5)]
@@ -139,7 +166,17 @@ def main():
         Flatten each block and arrange all the blocks in a (no_images*n_blocks_in_image) x (sz*sz) matrix called X
         '''
 
-        # TODO: Write a code snippet that performs as indicated in the above comment
+        n_blocks_in_image = int(height / sz) * int(width / sz)
+        X = np.empty([no_images*n_blocks_in_image, sz*sz], dtype="float32")
+
+        X_row_idx = 0
+        for i in range(0, no_images):
+            for j in range(0, int(height / sz)):
+                for k in range(0, int(width / sz)):
+                    block = all_imgs[i, j*sz : (j+1)*sz, k*sz : (k+1)*sz]
+                    block_flat = block.reshape([1, -1])
+                    X[X_row_idx, :] = block_flat
+                    X_row_idx += 1
 
         X_mean = np.mean(X, 0)
         X = X - np.repeat(X_mean.reshape(1, -1), X.shape[0], 0)
@@ -149,8 +186,11 @@ def main():
         matrix D
         '''
 
-        # TODO: Write a code snippet that performs as indicated in the above comment
+        cov = np.dot(X.T, X)
+        w, v = linalg.eig(cov)
 
+        order = w.argsort()[::-1] # Get indices of eigenvalues sorted in descending order
+        D = v[:, order]
         c = np.dot(D.T, X.T)
 
         for i in range(0, 200, 10):
